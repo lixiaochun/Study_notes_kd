505802a501d564f84b511d44c0429fe02d8c15c6
chenshangquan <2534920168@qq.com>
2020-06-17 17:46:47 +0800

    update
    update

--- Qt/samp4_7/mainwindow.cpp
+++ Qt/samp4_7/mainwindow.cpp
@@ -13,3 +13,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actListIni_triggered()
+{//初始化项
+    QListWidgetItem *aItem; //每一行是一个QListWidgetItem
+
+    QIcon aIcon;
+    aIcon.addFile(":/images/icons/check2.ico"); //设置ICON的图标
+    bool chk=ui->chkBoxListEditable->isChecked();//是否可编辑
+
+    ui->listWidget->clear(); //清除项
+    for (int i=0; i<10; i++)
+    {
+        QString str=QString::asprintf("Item %d",i);
+        aItem=new QListWidgetItem(); //新建一个项
+
+        aItem->setText(str); //设置文字标签
+        aItem->setIcon(aIcon);//设置图标
+        aItem->setCheckState(Qt::Checked); //设置为选中状态
+        if (chk) //可编辑, 设置flags
+            aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable |Qt::ItemIsUserCheckable |Qt::ItemIsEnabled);
+        else//不可编辑, 设置flags
+            aItem->setFlags(Qt::ItemIsSelectable |Qt::ItemIsUserCheckable |Qt::ItemIsEnabled);
+        ui->listWidget->addItem(aItem); //增加一个项
+    }
+}
